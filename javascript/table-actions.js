0a8e0f338fc5902e6ac0ce872728713a5d618017
caszhao <caszhao@163.com>
2023-03-29 21:18:54 +0800

    Add files via upload
    add base version V1.0

---
+++ javascript/table-actions.js
@@ -0,0 +1,317 @@
+
+
+function newElement(element, id, classList = [], label, nodeToAppend) {
+  const el = document.createElement(element);
+  el.id = id;
+  el.classList.add(...classList);
+  el.innerHTML = label;
+  return nodeToAppend.appendChild(el);
+}
+
+class TableActions {
+  constructor(element, options) {
+    this.table =
+      typeof element === "string" ? document.querySelector(element) : element;
+
+    this.tableRows = [
+      ...this.table.querySelector("tbody").querySelectorAll("tr"),
+    ];
+
+    this.currentPage = 0;
+
+    this.options = {
+      sortable: options.sortable ?? false,
+      paginable: options.paginable ?? true,
+      rowsPerPage: options.rowsPerPage ?? 10,
+      checkableRows: options.checkableRows ?? false,
+      checkableRowTdReference: options.checkableRowTdReference ?? "[data-ref]",
+      checkedElementsCallBack:
+        options.checkedElementsCallBack ??
+        function (checkedElements) {
+          console.log(checkedElements);
+        },
+    };
+
+    this._init();
+  }
+
+  _init() {
+    const checkableRows = this.options.checkableRows;
+    if (checkableRows) this._setTableCheckBoxes();
+    if (this.options.sortable) this._setTableSort(checkableRows);
+
+    if (this.options.paginable) {
+      this._setPaginationButtons();
+      this._updateTable();
+    }
+  }
+
+  _setPaginationButtons() {
+    const self = this;
+    const backButton = newElement(
+      "button",
+      "back-page",
+      ["btn", "btn-pagination"],
+      "<",
+      self.table.parentNode
+    );
+    const forwardButton = newElement(
+      "button",
+      "forward-page",
+      ["btn", "btn-pagination"],
+      ">",
+      self.table.parentNode
+    );
+
+    // Click button show all element selected
+    backButton.addEventListener("click", function (event) {
+      if (self.currentPage > 0) {
+        self.currentPage = self.currentPage -= 1;
+        self._updateTable();
+      }
+    });
+
+    forwardButton.addEventListener("click", function (event) {
+      if (self.currentPage < self._getLastPage()) {
+        self.currentPage = self.currentPage += 1;
+        self._updateTable();
+      }
+    });
+  }
+
+  _setTableSort(checkableRows) {
+    const self = this;
+
+    // Setting class to activate table arrows styles
+    this.table.classList.add("sortable");
+
+    const tableHeads = this.table.querySelectorAll("th");
+    for (
+      let thIndex = checkableRows ? 1 : 0; // if checkable column jump first
+      thIndex < tableHeads.length;
+      thIndex++
+    ) {
+      const th = tableHeads[thIndex];
+      const otherTh = [];
+
+      // Getting other columns to remove active icons class colors
+      for (const [index, el] of tableHeads.entries()) {
+        if (index !== thIndex) otherTh.push(el);
+      }
+
+      // Setting events listeners to get click in table headers.
+      // Clicks will activate sort to the clicked column
+      th.addEventListener("click", function () {
+        self._sortTable(th, thIndex, otherTh);
+      });
+    }
+  }
+
+  _sortDataFormat(format, val, nextVal) {
+    switch (format) {
+      case "DD/MM/YYYY":
+        val = val.split("/");
+        val = new Date(val[2] + "-" + val[1] + "-" + val[0]);
+        nextVal = nextVal.split("/");
+        nextVal = new Date(nextVal[2] + "-" + nextVal[1] + "-" + nextVal[0]);
+        break;
+
+      case "YYYY/MM/DD":
+        val = new Date(val.replace("/", "-"));
+        nextVal = new Date(nextVal.replace("/", "-"));
+
+      case "YYYY-MM-DD":
+        val = new Date(val);
+        nextVal = new Date(nextVal);
+        break;
+
+      case "YYYY-MM-DD HH:MM:SS":
+        const [valDate, valHour] = val.split(" ");
+        val = new Date(valDate + "T" + valHour);
+        const [nextValDate, nextValHour] = val.split(" ");
+        val = new Date(nextValDate + "T" + nextValHour);
+        break;
+
+      default:
+        throw new Error(`Format ${format} not recognized`);
+        break;
+    }
+
+    return [val, nextVal];
+  }
+
+  _sortTable(th, thIndex, otherThs) {
+    const self = this;
+    const asc = th.dataset.asc ? !JSON.parse(th.dataset.asc) : true;
+    const format = th.dataset.format;
+
+    for (const otherTh of otherThs) {
+      otherTh.removeAttribute("data-asc");
+    }
+
+    self.tableRows.sort(function (val, nextVal) {
+      val = val.querySelectorAll("td")[thIndex].innerHTML;
+      nextVal = nextVal.querySelectorAll("td")[thIndex].innerHTML;
+
+      if (format) {
+        [val, nextVal] = self._sortDataFormat(format, val, nextVal);
+      } else {
+        const regex = /[\ \,\;\n]/g;
+
+        val = val.replace(regex, "").toLowerCase();
+        nextVal = nextVal.replace(regex, "").toLowerCase();
+
+        if (!isNaN(val)) {
+          val = parseFloat(val);
+          nextVal = parseFloat(nextVal);
+        } else {
+          val = toNormalForm(val);
+          nextVal = toNormalForm(nextVal);
+        }
+      }
+
+      if ((asc && val > nextVal) || (!asc && val < nextVal)) {
+        return 1;
+      }
+      if ((asc && val < nextVal) || (!asc && val > nextVal)) {
+        return -1;
+      }
+
+      // value must be equal to nextValue
+      return 0;
+    });
+
+    // Adding sorted elements to the table body
+    self._updateTable();
+
+    // Updating dataset asc value
+    th.dataset.asc = asc;
+  }
+
+  _setTableCheckBoxes() {
+    // Get class reference to actual table
+    const self = this;
+
+    function tableCheckboxInsert(elementType, classes = []) {
+      const element = document.createElement(elementType);
+      const input = document.createElement("input");
+
+      if (classes.length) {
+        element.classList.add(...classes);
+      }
+
+      input.type = "checkbox";
+      element.appendChild(input);
+
+      return element;
+    }
+
+    // Add table header checkbox
+    const tr = self.table.querySelector("thead>tr");
+    tr.prepend(tableCheckboxInsert("th", ["tb-checkbox-column"]));
+
+    // Add table rows checkbox
+    for (const tr of self.table.querySelectorAll("tbody>tr")) {
+      tr.prepend(tableCheckboxInsert("td", ["tb-checkbox-row"]));
+    }
+
+    // Set interaction button
+    const button = newElement(
+      "button",
+      "interact",
+      ["btn"],
+      "Interact",
+      self.table.parentNode
+    );
+
+    // Click button show all element selected
+    button.addEventListener("click", function (event) {
+      const checked = [];
+      for (const row of self.tableRows) {
+        if (row.querySelector("[type='checkbox']").checked) {
+          checked.push(
+            row.querySelector(self.options.checkableRowTdReference).dataset.ref
+          );
+        }
+      }
+
+      self.options.checkedElementsCallBack(checked);
+    });
+
+    // Table checkboxes logic, check all and check one by one
+    var checkboxes = document.querySelectorAll("[type='checkbox']");
+
+    for (const checkbox of checkboxes) {
+      checkbox.addEventListener("click", function (event) {
+        const thead = event.target.closest("thead");
+
+        if (thead) {
+          for (const el of self.tableRows) {
+            const checkbox = el.querySelector("[type='checkbox']");
+            if (event.target.checked) {
+              checkbox.checked = true;
+              checkbox.closest("tr").classList.add("checked");
+            } else {
+              checkbox.checked = false;
+              checkbox.closest("tr").classList.remove("checked");
+            }
+          }
+        } else {
+          self.table.querySelector("thead [type='checkbox']").checked = false;
+          event.target.closest("tr").classList.toggle("checked");
+        }
+      });
+    }
+  }
+
+  _getCurrentStartRow() {
+    return this.options.rowsPerPage * this.currentPage;
+  }
+
+  _getLastPageRow() {
+    return this._getCurrentStartRow() + this.options.rowsPerPage;
+  }
+
+  _getLastPage() {
+    return Math.ceil(this.tableRows.length / this.options.rowsPerPage) - 1;
+  }
+
+  _updateTable() {
+    const self = this;
+    const tbody = self.table.querySelector("tbody");
+
+    if (self.options.paginable) {
+      tbody.innerHTML = "";
+      for (
+        let i = self._getCurrentStartRow();
+        i < self._getLastPageRow() && i < self.tableRows.length;
+        i++
+      ) {
+        tbody.appendChild(self.tableRows[i]);
+      }
+
+      // Update buttons state
+      self._updateButtons();
+    } else {
+      for (const row of self.tableRows) {
+        tbody.appendChild(row);
+      }
+    }
+  }
+
+  _updateButtons() {
+    const self = this;
+
+    if (self.currentPage === self._getLastPage()) {
+      self.table.parentNode.querySelector("#forward-page").disabled = true;
+    } else {
+      self.table.parentNode.querySelector("#forward-page").disabled = false;
+    }
+
+    if (self.currentPage === 0) {
+      self.table.parentNode.querySelector("#back-page").disabled = true;
+    } else {
+      self.table.parentNode.querySelector("#back-page").disabled = false;
+    }
+  }
+}
---
+++ server_websocket.py
@@ -0,0 +1,239 @@
+#coding=utf-8
+
+import asyncio
+import json
+import os
+import websockets
+import random
+import subprocess
+import socket
+import time
+import psutil
+import glob
+
+refresh_interval = 30
+
+
+def get_mounted_devices():
+    # 获取已挂载的设备节点
+    try  :
+        output = subprocess.check_output(['lsblk', '-o', 'NAME,MOUNTPOINT,TYPE', '-r']).decode('utf-8')
+        lines = output.strip().split('\n')
+    except Exception as e :
+        print(e)
+        print("Error Command: lsblk -o NAME,MOUNTPOINT,TYPE")
+
+    devices = {}
+    for line in lines:
+        parts = line.strip().split()
+        if len(parts) == 3 and parts[1].startswith("/") and (not parts[1].startswith("/boot")) and (parts[2] == "disk" or parts[2] == "part"):
+            devices[parts[0]] = parts[1]
+    return devices
+
+def get_disk_info(device):
+
+    print('====================get_disk_info++=====================')
+
+    # 获取硬盘信息
+    lines = ""
+    try  :
+        output = subprocess.check_output(['smartctl', '-i', '/dev/' + device]).decode('utf-8')
+        lines = output.strip().split('\n')
+    except Exception as e :
+        print(e)
+        print("Error Command: smartctl -i /dev/" + device)
+    
+    info = {}
+    info['model'] = "N/A"
+    info['serial'] = "N/A"
+    info['temperature'] = "N/A"
+    info['capacity'] = "N/A"
+    info['health'] = "Warning"
+
+
+    for line in lines:
+        parts = line.strip().split(':')
+        if len(parts) == 2:
+
+            key = parts[0].strip()
+            value = parts[1].strip()
+            # == 'Device Model'
+            if 'Model' in key:
+                info['model'] = value
+            elif key == 'Serial Number':
+                info['serial'] = value
+    
+    try  :
+        output = subprocess.check_output(['hddtemp', '-n', '/dev/' + device]).decode('utf-8')
+        lines = output.strip().split('\n')
+        
+    except Exception as e :
+        print("Error Command: hddtemp -n /dev/"+device)
+
+    for line in lines:
+        parts = line.strip().split(':')
+        if len(parts) == 1 and len(parts[0].strip()) < 3 and parts[0].strip().isdigit():
+            info['temperature'] = parts[0].strip()
+
+    # Health
+    output = subprocess.check_output(['smartctl', '-H', '/dev/' + device]).decode('utf-8')
+    lines = output.strip().split('\n')
+    devices = {}
+    for line in lines:
+        if 'PASSED' in line:
+            info['health'] = 'Health'
+
+    print('====================get_disk_info--=====================')
+    return info
+
+def get_root_plot_files(path):
+    # 获取硬盘根目录下的.plot文件数量
+    plot_files = glob.glob(os.path.join(path, "*.plot"))
+    return len(plot_files)
+
+def get_host_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('8.8.8.8', 80))  # 114.114.114.114也是dns地址
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+    return ip
+
+# 获取CPU使用率
+def get_cpu_usage():
+    return psutil.cpu_percent(interval=3)
+
+# 获取内存使用率和已用内存/总内存
+def get_memory_usage():
+    mem = psutil.virtual_memory()
+    return mem.percent, mem.used / 1024 / 1024 / 1024, mem.total / 1024 / 1024 /1024
+
+
+def getHumanSize(x):
+    if x < 1024*1024:
+        return str(round(x / 1024.0, 1)) + "M"
+    elif x < 1024*1024*1024:
+        return str(round(x / 1024.0 / 1024.0, 1)) + "G"
+    else:
+        return str(round(x / 1024.0 / 1024.0 / 1024.0, 1)) + "T"
+    
+    return "N/A"
+
+
+
+async def send_json(websocket, path):
+    while True:
+        global refresh_interval
+        time_interval = await websocket.recv()
+        print("Server received: ", time_interval)
+        if time_interval.isdigit():
+            refresh_interval = int(time_interval)
+            print("Server received : set refresh_interval = ", refresh_interval)
+
+        monitor_data ={"Machines": [], 
+                        'Disks': []}
+
+        print("*******************************************")
+        devices = get_mounted_devices()
+        info={}
+        Disks = []
+        total_plot_num = 0
+        inx = 0
+        for device in devices:
+            if os.path.exists("/dev/" + device):
+                #print("-----device:------")
+                #print("/dev/" + device)
+                mount_point = devices[device]
+                #print("-----mountpoint:------")
+                #print(mount_point)
+                
+                info = get_disk_info(device)
+                info['device']='/dev/' + device
+                info['mountpoint'] = mount_point
+                print('====================info=====================')
+                print(info)
+                print('====================info end=====================')
+                
+                print('Device: ' + info['device'])
+                print('Mount point: ' + info['mountpoint'])
+                print('Model: ' + info['model'])
+                print('Serial: ' + info['serial'])
+                print('Capacity: ' + info['capacity'])
+                print('Temperature: ' + info['temperature'])
+                print('Health: ' + info['health'])
+
+                if mount_point != '':
+                    output = subprocess.check_output(['df', mount_point]).decode('utf-8')
+                    lines = output.strip().split('\n')
+                    parts = lines[1].strip().split()
+                    if len(parts) == 6:
+                        total = 0
+                        used = 0
+                        usage = 0
+
+                        if  parts[1].isdigit():
+                            total = int(parts[1])
+
+                        if parts[2].isdigit():
+                            used = int(parts[2])
+                        
+                        info['capacity'] = getHumanSize(total)
+                        info['used'] = getHumanSize(used)
+                        info['usage'] = parts[4]
+                        
+                        plot_num = get_root_plot_files(mount_point)
+                        total_plot_num += plot_num
+                        info['plot'] = str(plot_num)
+                        #total = parts[1]
+
+
+                        print('Used capacity: ' + info['used'])
+                        print('Usage percentage: ' + info['usage'])
+                        print('Number of .plot : ' + info['plot'])
+                        
+                        inx += 1
+                        Disks.append({"ID": str(inx), 
+                                    "IP": local_ip, 
+                                    "Serial": info['serial'], 
+                                    "DeviceName": info['device'], 
+                                    "Model": info['model'], 
+                                    "Size": info['capacity'], 
+                                    "Used": info['used'], 
+                                    "Usaged": info['usage'], 
+                                    "NumOfPlot": info['plot'], 
+                                    "Temp": info['temperature'], 
+                                    "Path": info['mountpoint'], 
+                                    "Healthy": info['health'], 
+                                    "GetDateTime": time.strftime("%Y-%m-%d %H:%M:%S")})
+        print("*******************************************")
+
+        monitor_data['Disks'] = Disks
+
+        mem_percent, mem_used, mem_total = get_memory_usage()
+
+        Machines = []
+
+        Machines.append({"IP": local_ip, 
+                        "HardDiskNum": str(inx), 
+                        "CpuUsaged": str(get_cpu_usage())+"%", 
+                        "MemUsaged": str(mem_percent) + "%",
+                        "MemUsed": str(round(mem_used,1)) + " GB/" + str(round(mem_total,1))+ "GB", 
+                        "TotalPlot": str(total_plot_num), 
+                        "GetDateTime": time.strftime("%Y-%m-%d %H:%M:%S")})
+        monitor_data['Machines'] = Machines
+
+        message = json.dumps(monitor_data)
+        await websocket.send(message)
+        print("Server sent: ", monitor_data)
+
+        print("refresh_interval : ", refresh_interval)
+        await asyncio.sleep(refresh_interval)
+
+
+local_ip=get_host_ip()
+
+start_server = websockets.serve(send_json, local_ip, 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
